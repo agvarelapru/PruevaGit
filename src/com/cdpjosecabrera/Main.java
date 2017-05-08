2b34343f228275493caeb7adb1d217bcf68aa97b
agvarelapru <agvarelapru@gmail.com>
2017-05-08 17:53:23 +0200

    Main suma

--- src/com/cdpjosecabrera/Main.java
+++ src/com/cdpjosecabrera/Main.java
@@ -4,6 +4,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Apéndice de método generado automáticamente
+		
+		Calculadora calcu=new Calculadora();
+		int suma=calcu.suma(5, 20);
+		
+		System.out.println(suma);
+		
+		
 
 	}
 
